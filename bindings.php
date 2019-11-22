e6e1935d46046586737932a8edb1c9aa80a32aca
rastatech <todd.hochman@state.nm.us>
2019-11-22 15:54:15 -0700

    Arrayed Bindings Chapter 2 - the Revenge of the array! Had to completely rework how I was testing for the array type to bind; hopefully working; this commit is post-kruft removal, hopefully i didn't break anything....

--- bindings.php
+++ bindings.php
@@ -2,8 +2,7 @@
 
 namespace rastatech\odbal;
 
-use ArrayObject;
-use \DateTime;
+use \ArrayObject;
 use \Exception;
 
 /**
@@ -47,7 +46,19 @@ class bindings
      */
     public $cursors2bind = array();
 
+    /**
+     * @var string-array    the array of keys used in defining attributes for OUT parameter binding
+     */
     protected $_compound_value_keys = array('length' => NULL, 'type' => NULL, 'value' => NULL);
+
+    /**
+     * @var string-array    the array of regexes to use to parse for arrayed paramter bind types; trickier than it sounds....
+     */
+    protected $_regexes = ['float' => '@-?\d+\.?\d*[eE][- +]\d+@',
+                            'num' => '@^-?\d+\.\d*$@',
+                            'int' => '@^-?\d+$@',
+                            'date' => '@(((\d{2}-[A-Z]{3}-\d{2})|(\d{2}/\d{2}/\d{2}(?:\d{2})?)|(\d{4}[-.]\d{2}[-.]\d{2})))+@'
+                            ];
 
     /**
      * Configuration loading Trait
@@ -67,9 +78,6 @@ class bindings
     {
         $this->ci = $ci;
         $loadedConfigs = $this->_get_configs($model_sql_elements);
-//        var_dump($loadedConfigs);
-//        echo "dumping the binding configs to see if we have access to cursor array<br/>";
-//        die('so...');
         $this->_assign2classVars($loadedConfigs);
     }
 
@@ -97,9 +105,6 @@ class bindings
             $return = (!$stmt) ? $this->vars2bind : $this->_bindVars2SQL($stmt, $sqlType);
         }
         $return = ((!isset($return)) AND ($stmt)) ? $this->_bindVars2SQL($stmt, $sqlType) : $return;
-//        echo "dumping the bindings...";
-//        var_dump($return);
-//        die('<br/> &^&^&^&^&^& end of bind vars;' );
         return $return;
     }
 
@@ -119,11 +124,7 @@ class bindings
         }
         if ($this->vars2bind) {
             $b = ($sqlType < 2) ? $this->_bind_pkg($stmt) : $this->bind_passThruSQL($stmt); //not currently supported but leaving it open....
-//            $this->bound_vars = ($b) ? $this->vars2bind : []; //this makes sure we have an empty array for bound_vars at the very least so nothing explodes
             $this->bound_vars = $b;
-//            var_dump( $this->bound_vars);//
-//            echo "is the bound vars from bindings;";
-//            die('pleasee please');
             return $b;
         }
     }
@@ -147,16 +148,12 @@ class bindings
             throw new Exception('no valid OCI statement received in _bind_pkg', 520);
         }
         $b = array();
-//        var_dump($this->vars2bind);
-//        die('raw vars 2 bind');
         foreach ($this->vars2bind as $bind_var => $bind_value) {
             $this->bound_vars[$bind_var] = $bind_value;
 //            //dynamically add a public class attribute by which to access the bound var:
             $this->$bind_var = ((is_array($bind_value)) AND (array_key_exists('value', $bind_value))) ? $bind_value['value'] : $bind_value;
             $b[$bind_var] = $this->_bind_pkg_parameter($stmt, $bind_var, $bind_value);
         }
-//        var_dump($b);
-//        die('vars bound!');
         return $b;
     }
 
@@ -205,49 +202,25 @@ class bindings
     {
         $placeholder = [];
         $placeholder[$bind_var] = ":$bind_var";
-//        echo "for $bind_var, the value is " . var_export($bind_value, TRUE) . "<br/>";
-        //is it an (out_var or return_var), or an arrayed value?
         $is_outvar = $this->_is_outVar($bind_var);
-//        echo "$bind_var " . (($is_outvar) ? "is" : "isn't") . " an outvar!<br/>";
         $this->$bind_var = $bind_value;
-//        if (is_array($bind_value)){
-//            echo "$bind_var is an array!<br/>";
-//        }
-//        else{
-//            echo "$bind_var is NOT an array!<br/>";
-//        }
-//        return;
         if($is_outvar){//test for out/return var-ness
-//            die('found out var!');
-//        echo "parsing attributes for $bind_var: <br/>";
             $binding_info = $this->_parse_outVar_4attribs($bind_value);
-//            var_dump($binding_info);
-//            die('outvar binding info');
             $this->$bind_var = $binding_info['value'];
             //only need set length & type for OUT params:
             $boundVar = oci_bind_by_name($stmt, $placeholder[$bind_var], $this->$bind_var, $binding_info['length'], $binding_info['type']);
-//            var_dump($boundVar);
-//            die('bindvar ' . $bind_var);
         }
         elseif (is_array($bind_value)){//test for arrayed value-ness
             $binding_info = $this->_parse4arrayINparam($bind_value);
             if((array_key_exists('value', $binding_info)) AND ($binding_info['value'] != $this->$bind_var)){
                 $this->$bind_var = $binding_info['value']; //provide a means to fix positive exponents in floats that Slim turns into spaces from the parameter
-//                echo "using massaged  value of " . $this->$bind_var . "<br/>\n";
             }
-//            echo "binding array: $bind_var<br/>";
-//            echo "parsing attributes for $bind_var: <br/>";
-//            var_dump($binding_info);
-//            echo 'type is ' . $binding_info['type'];
-//            die("done parsing arrayed params!<br/>\n");
             $boundVar = oci_bind_array_by_name($stmt, $placeholder[$bind_var], $this->$bind_var, $binding_info["length"]['max_table_length'], $binding_info["length"]['max_item_length'], $binding_info['type']);
         }
         else{ //otherwise it's a normal IN param; act accordingly:
-//            echo "binding normal value: $bind_var with value " . $this->$bind_var . "<br/>";
             $boundVar = oci_bind_by_name($stmt, $placeholder[$bind_var], $this->$bind_var); //let oracle decide length and type for IN parameters
         }
         if ($o_err = oci_error($stmt)) {
-//            die('oci_error. :(');
             $this->_throwBindingError($o_err, $bind_var, 'Oracle bind by name failed!', 523);
         }
         return $boundVar;
@@ -278,7 +251,6 @@ class bindings
      */
     protected function _is_outVar($bind_var)
     {
-//        var_dump($this->ci->configs);
         $outVar_suffixes = $this->ci->configs['out_params'];
         $funcRet_suffixes = $this->ci->configs['function_return_params'];
         $check_suffixes = array_merge($outVar_suffixes, $funcRet_suffixes);
@@ -288,7 +260,6 @@ class bindings
                 return TRUE;
             }
         }
-//        die('so far so good...');
     }
 
     /**
@@ -323,8 +294,6 @@ class bindings
     {
         $parsedAttributes = $this->_compound_value_keys;
         $parsedKeys = array_keys($parsedAttributes);
-//        echo "dumping the keys to iterate thru: ";
-//        var_dump($parsedKeys);
         foreach ($parsedKeys as $key){
             $parsedAttributes[$key] = $this->_parseCompoundValues($bind_value,$key);
             if($parsedAttributes[$key] === FALSE){
@@ -345,9 +314,7 @@ class bindings
      */
     protected function _parseCompoundValues($bind_value, $key)
     {
-//        echo "parsing bind value of " . var_export($bind_value, TRUE) . " for $key<br/>";
         if ((is_array($bind_value)) AND  (array_key_exists($key,$bind_value))){//compound values should tell us what to set for each parameter
-//            echo "bind value is array! checking for compoundedness for $key...<br/>";
             switch ($key){
                 case "type":
                     if( ! is_numeric($bind_value[$key])){
@@ -355,26 +322,20 @@ class bindings
                         $type = 'SQLT_CHR'; //was giving weird results w/ SQLT_NUM or really anything else for return vars
                         $uc_type = strtoupper($type);
                         $parsedKey = constant($uc_type);
-//                        echo 'parsed string-based  compound type successfully! Type is ' . $uc_type . "<br/>";
                         break;
                     }
-//                    echo 'parsed numeric compound type successfully! Type is ' . $bind_value[$key] . "<br/>";
                     $parsedKey = $bind_value[$key];
                     break;
                 case "length":
                     $length = (is_int($bind_value[$key])) ? $bind_value[$key] : (int)$bind_value[$key];
-//                    echo 'parsed compound length successfully! length is ' . $length . "<br/>";
                     $parsedKey = $length;
                     break;
                 case "value":
-//                    echo 'parsed compound value successfully! value is ' . $bind_value[$key] . "<br/>";
                     $parsedKey = $bind_value[$key];
                     break;
                 default:
-//                    echo 'skipped all cases with ' . $key . "<br/>";
                     $parsedKey = FALSE;
             }
-//            echo "parsed compound value successfully for $key! $key is " . $parsedKey . "<br/>";
             return $parsedKey;
         }
         return FALSE;
@@ -389,16 +350,8 @@ class bindings
      */
     protected function _parse_bindVar_4type($bind_valueArray, $parsedAttributes)
     {
-//        die('parsing');
-        $floatClues = [''];
         $arrObj = new ArrayObject($bind_valueArray);
         $arrIterator = $arrObj->getIterator();
-//        echo "iterating....<br/>";
-        $regexes = ['float' => '@-?\d+\.?\d*[eE][- +]\d+@',
-                    'num' => '@^-?\d+\.\d*$@',
-                    'int' => '@^-?\d+$@',
-                    'date' => '@(((\d{2}-[A-Z]{3}-\d{2})|(\d{2}/\d{2}/\d{2}(?:\d{2})?)|(\d{4}[-.]\d{2}[-.]\d{2})))+@'
-                    ];
         $valueMatches = ['float' => 0,
                         'num' => 0,
                         'int' => 0,
@@ -406,33 +359,26 @@ class bindings
                         'varchar' => 0
                         ];
         while($arrIterator->valid()) {
-//            echo $arrIterator->key() . ' => ' . $arrIterator->current() . "<br/>\n";
             $current = trim($arrIterator->current(),'"\''); //trim any quotes
             $matches = 0;
-            foreach ($regexes as $regexkey => $regexPattern){
+            foreach ($this->_regexes as $regexkey => $regexPattern){
                 $matching =  preg_match($regexPattern, $current, $matches);
                 if($matching){
-//                    echo "woohoo! matching $regexkey<br/>";
                     switch ($regexkey){
                         case 'float':
                             $valueMatches['float']++;
                             if(strpos($current, ' ')){
                                 $parsedAttributes['value'] = str_replace(' ', '+', $current);
-//                                echo "fixed the missing positive exponent!<br/>\n";
                             }
-//                            echo "floating away.... matching $regexkey with value of " . $valueMatches['float'] . "<br/>";
                             break;
                         case 'num':
                             $valueMatches['num']++;
-//                            echo "Numnums! matching $regexkey with value of " . $valueMatches['num'] . "<br/>";
                             break;
                         case 'int':
                             $valueMatches['int']++;
-//                            echo "INTeresting...  matching $regexkey with value of " . $valueMatches['int'] . "<br/>";
                             break;
                         case 'date':
                             $valueMatches['date']++;
-//                            echo "Now your're talkin'!  matching $regexkey with value of " . $valueMatches['date'] . "<br/>";
                             break;
                     }
                     $matches++;
@@ -441,27 +387,22 @@ class bindings
             }
             if( ! $matches){
                 $valueMatches['varchar']++;
-//                echo "uhoh! VarChar Territory.... matching $regexkey with value of " . $valueMatches['varchar'] . "<br/>";
             }
             $arrIterator->next();
         }
         //hierarchy of types
         if($valueMatches['varchar']){
-//            echo  "type is varchar <br/>\n";
             $parsedAttributes['type'] = SQLT_CHR;
             return $parsedAttributes;
         }
         $numArray = array($valueMatches['float'], $valueMatches['num'], $valueMatches['int']);
         $hasNumsToo = count(array_filter($numArray));
         if(( ! $hasNumsToo) AND $valueMatches['date']){
-//            echo  "type is date <br/>\n";
             $parsedAttributes['type'] = SQLT_ODT;
             return $parsedAttributes;
         }
         if($hasNumsToo){
             $numType = ($valueMatches['float']) ? SQLT_FLT : (($valueMatches['num']) ? SQLT_NUM : SQLT_INT);
-            $msg = ($numType == SQLT_FLT) ? 'float' : (($numType == SQLT_NUM) ? 'num' : 'int');
-//            echo "type is  $msg  <br/>\n";
             $parsedAttributes['type'] = $numType;
             return $parsedAttributes;
         }
